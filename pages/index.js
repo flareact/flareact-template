320dce1b81d4f11be108aba5672f782d279cf42d
Josh Larson <jplhomer@gmail.com>
2020-08-07 08:58:09 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Index() {
+  return (
+    <h1
+      style={{
+        fontSize: "2em",
+        fontFamily:
+          '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+        textAlign: "center",
+        margin: "4em"
+      }}
+    >
+      You're running React on the Edge!
+    </h1>
+  );
+}
