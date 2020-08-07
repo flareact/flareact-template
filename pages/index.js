3010e86378537de8839109e67e9eb0dae862720d
Josh Larson <jplhomer@gmail.com>
2020-08-07 09:00:35 -0500

    Remove liquid breaking things

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,6 @@
 export default function Index() {
   return (
-    <h1
-      style={{
-        fontSize: "2em",
-        fontFamily:
-          '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-        textAlign: "center",
-        margin: "4em"
-      }}
-    >
+    <h1>
       You're running React on the Edge!
     </h1>
   );
